7def2cd410520d5582944046dcd923e4aebc88ab
Fahri Maulana <88439222+fahrimaulanaa@users.noreply.github.com>
2022-04-26 21:23:43 +0700

    Bug fixed

--- ScriptAbsen.py
+++ ScriptAbsen.py
@@ -1,5 +1,6 @@
 from psycopg2 import Time
 import py
+import filewriter
 from selenium import webdriver
 from datetime import datetime
 from selenium.webdriver.chrome.options import Options
@@ -24,18 +25,18 @@ time_now = datetime.now(WIB)
 while True:
     WIB = pytz.timezone('Asia/Jakarta')
     time_now = datetime.now(WIB)
-    print("# Menunggu pukul 06:00 WIB")
+    print("<----Menunggu pukul 06:00 WIB---->")
 
-    if time_now.strftime('%H') == '22' and time_now.strftime('%M') == '26':
+    if time_now.strftime('%H') == '06' and time_now.strftime('%M') == '00':
         try:
             driver.refresh()
             masuk = driver.find_element_by_xpath('//label[@for="M"]')
             masuk.click()
 
-            daring = WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.XPATH, ".//*[contains(text(), 'DARING')]")))
+            daring = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, ".//*[contains(text(), 'DARING')]")))
             daring.click()
 
-            simpan = WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.XPATH, ".//*[contains(text(), 'SIMPAN')]")))
+            simpan = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, ".//*[contains(text(), 'SIMPAN')]")))
             simpan.click()
         
             alert = driver.switch_to.alert
@@ -50,6 +51,7 @@ while True:
         except:
             print("ABSEN GAGAL PADA ", time_now.strftime('%H:%M'), "WIB") 
             pass
+        driver.close()
         break
 
 
